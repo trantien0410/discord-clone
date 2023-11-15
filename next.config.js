63c6e5118fc3e641a9fc0e4b43a7750a8d073856
Tien <tien04100@gmail.com>
2023-11-15 17:40:03 +0700

    modify nextjs config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["uploadthing.com", "utfs.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
